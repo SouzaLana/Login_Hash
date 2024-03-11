a4d4361546bc3ca80ac9e72b50939744779ba06e
CJ3016714 <CJ3016714@LAB01-PC03>
2024-03-11 09:10:36 -0300

    Adicionar arquivos de projeto.

---
+++ Login_Hash/Form1.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace Login_Hash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.Login = new System.Windows.Forms.TabPage();
+            this.Registro = new System.Windows.Forms.TabPage();
+            this.Senha = new System.Windows.Forms.Label();
+            this.Usuário = new System.Windows.Forms.Label();
+            this.txtSenhaLogin = new System.Windows.Forms.TextBox();
+            this.txtUsuarioLogin = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Usuario = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtConfirmarSenha = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tabControl1.SuspendLayout();
+            this.Login.SuspendLayout();
+            this.Registro.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.Login);
+            this.tabControl1.Controls.Add(this.Registro);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(501, 270);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // Login
+            // 
+            this.Login.Controls.Add(this.pictureBox1);
+            this.Login.Controls.Add(this.btnLogin);
+            this.Login.Controls.Add(this.txtUsuarioLogin);
+            this.Login.Controls.Add(this.txtSenhaLogin);
+            this.Login.Controls.Add(this.Senha);
+            this.Login.Controls.Add(this.Usuário);
+            this.Login.Location = new System.Drawing.Point(4, 22);
+            this.Login.Name = "Login";
+            this.Login.Padding = new System.Windows.Forms.Padding(3);
+            this.Login.Size = new System.Drawing.Size(493, 244);
+            this.Login.TabIndex = 0;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = true;
+            // 
+            // Registro
+            // 
+            this.Registro.Controls.Add(this.pictureBox2);
+            this.Registro.Controls.Add(this.btnRegistrar);
+            this.Registro.Controls.Add(this.txtSenha);
+            this.Registro.Controls.Add(this.txtConfirmarSenha);
+            this.Registro.Controls.Add(this.txtEmail);
+            this.Registro.Controls.Add(this.txtUsuario);
+            this.Registro.Controls.Add(this.label4);
+            this.Registro.Controls.Add(this.label3);
+            this.Registro.Controls.Add(this.label2);
+            this.Registro.Controls.Add(this.Usuario);
+            this.Registro.Location = new System.Drawing.Point(4, 22);
+            this.Registro.Name = "Registro";
+            this.Registro.Padding = new System.Windows.Forms.Padding(3);
+            this.Registro.Size = new System.Drawing.Size(493, 244);
+            this.Registro.TabIndex = 1;
+            this.Registro.Text = "Registro";
+            this.Registro.UseVisualStyleBackColor = true;
+            // 
+            // Senha
+            // 
+            this.Senha.AutoSize = true;
+            this.Senha.Location = new System.Drawing.Point(73, 137);
+            this.Senha.Name = "Senha";
+            this.Senha.Size = new System.Drawing.Size(38, 13);
+            this.Senha.TabIndex = 1;
+            this.Senha.Text = "Senha";
+            // 
+            // Usuário
+            // 
+            this.Usuário.AutoSize = true;
+            this.Usuário.Location = new System.Drawing.Point(73, 64);
+            this.Usuário.Name = "Usuário";
+            this.Usuário.Size = new System.Drawing.Size(43, 13);
+            this.Usuário.TabIndex = 2;
+            this.Usuário.Text = "Usuário";
+            this.Usuário.Click += new System.EventHandler(this.Usuário_Click);
+            // 
+            // txtSenhaLogin
+            // 
+            this.txtSenhaLogin.Location = new System.Drawing.Point(136, 137);
+            this.txtSenhaLogin.Name = "txtSenhaLogin";
+            this.txtSenhaLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtSenhaLogin.TabIndex = 3;
+            // 
+            // txtUsuarioLogin
+            // 
+            this.txtUsuarioLogin.Location = new System.Drawing.Point(136, 64);
+            this.txtUsuarioLogin.Name = "txtUsuarioLogin";
+            this.txtUsuarioLogin.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuarioLogin.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(119, 208);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(75, 23);
+            this.btnLogin.TabIndex = 5;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(323, 76);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Usuario
+            // 
+            this.Usuario.AutoSize = true;
+            this.Usuario.Location = new System.Drawing.Point(23, 30);
+            this.Usuario.Name = "Usuario";
+            this.Usuario.Size = new System.Drawing.Size(43, 13);
+            this.Usuario.TabIndex = 0;
+            this.Usuario.Text = "Usuário";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Senha";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Confirmar";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(93, 30);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtUsuario.TabIndex = 4;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(93, 117);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(100, 20);
+            this.txtEmail.TabIndex = 5;
+            // 
+            // txtConfirmarSenha
+            // 
+            this.txtConfirmarSenha.Location = new System.Drawing.Point(93, 90);
+            this.txtConfirmarSenha.Name = "txtConfirmarSenha";
+            this.txtConfirmarSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtConfirmarSenha.TabIndex = 6;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Location = new System.Drawing.Point(93, 60);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(100, 20);
+            this.txtSenha.TabIndex = 7;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(52, 164);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 8;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(297, 60);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.Login.ResumeLayout(false);
+            this.Login.PerformLayout();
+            this.Registro.ResumeLayout(false);
+            this.Registro.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage Login;
+        private System.Windows.Forms.TabPage Registro;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtUsuarioLogin;
+        private System.Windows.Forms.TextBox txtSenhaLogin;
+        private System.Windows.Forms.Label Senha;
+        private System.Windows.Forms.Label Usuário;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Usuario;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtConfirmarSenha;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtUsuario;
+    }
+}
+
---
+++ Login_Hash/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace Login_Hash
+{
+    public static class Crypton
+    {
+        public static string md5encrypt(string frase)
+        {
+            UTF8Encoding encoding = new UTF8Encoding();
+            MD5CryptoServiceProvider mD5CryptoServiceProvider = new MD5CryptoServiceProvider();
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Usuário_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario = txtUsuario.Text;
+            string senha = Crypto.sha256encrypt(txtSenhaLogin.Text)
+        }
+    }
+}
